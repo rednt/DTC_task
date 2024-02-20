31cad298ab2ef1ba84562c14e6203a53befdbef0
rednt <souhail200ak@gmail.com>
2024-02-20 18:33:20 +0100

    summoy function

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 let person = {nom:"Alex",adress:"Paris",age:25};
 
-function getInput(){
-    
+let valeurs = [];
+let somme = 0;
+let moy = 0.0;
+
+function summoy(){
+    valeurs = prompt("Entrer des valeurs séparée par ',':").split(',').map(Number);
+    for(let i = 0; i < valeurs.length;i++){
+        somme += valeurs[i];
+    }
+    moy = somme/valeurs.length;
+    document.getElementById('vals').innerHTML = "Les valeurs: "+valeurs.join(" ");
+    document.getElementById('dispSum').innerHTML = "Somme: "+somme;
+    document.getElementById('dispMoy').innerHTML = "Moyenne: "+moy;
 }
 
-function sommoy(){
 
-}
+
+
